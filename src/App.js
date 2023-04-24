7325aac94f010abe4ea58f95455066f03a41243d
shreyasnarke <narkeshreyas44@gmail.com>
2023-04-24 15:25:21 +0530

    Initially Committttt

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <Header></Header>
+     <Footer></Footer>
+    </Router>
   );
 }
 
 export default App;
-// echo "# my-portfolio-react" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin git@github.com:shreyasnarke/my-portfolio-react.git
-// git push -u origin main
